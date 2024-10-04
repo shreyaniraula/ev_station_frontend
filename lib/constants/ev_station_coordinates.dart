690951d576ec78993eeabbc5a7b4f152931b435f
Bibesh17 <bibeshpokharel17@gmail.com>
2024-10-04 21:16:15 +0545

    biratnagar updated

--- lib/constants/ev_station_coordinates.dart
+++ lib/constants/ev_station_coordinates.dart
@@ -1,87 +1,9 @@
 List evStationsCoordinates = [
   {
-    'name': 'NEA Charging Station',
-    'latitude': 26.465676901405185,
-    'longitude': 87.28514082612476,
-    'address': 'Koshi Road, Biratnagar, Nepal'
-  },
-  {
-    'name': 'Kathmandu Ev Station',
-    'latitude': 27.7172,
-    'longitude': 85.3240,
-    'address': 'Thamel, Kathmandu, Nepal'
-  },
-  {
-    'name': 'KMC EV station',
-    'latitude': 27.68459852890479,
-    'longitude': 85.41120564749042,
-    'address': 'Ward 18, Kathmandu'
-  },
-  {
-    'name': 'Biratnagar station',
-    'latitude': 26.491221894767232,
-    'longitude': 87.26459476011038,
-    'address': 'Biratnagar-16, Morang, Nepal'
-  },
-  {
-    'name': 'Civil Service EV station',
-    'latitude': 27.68656625538563,
-    'longitude': 85.33866987195383,
-    'address': 'Putalisadak, Kathmandu, Nepal'
-  },
-  {
-    'name': 'New Amda EV station',
-    'latitude': 26.679091070440393,
-    'longitude': 87.6867315522346,
-    'address': 'Amda, Jhapa, Nepal'
-  },
-  {
-    'name': 'Chandragiri EV station',
-    'latitude': 26.576475130500278,
-    'longitude': 88.07778683051427,
-    'address': 'Chandragiri Hills, Thankot, Nepal'
-  },
-  {
-    'name': 'Pathivara EV station',
-    'latitude': 26.57499655512588,
-    'longitude': 87.8302896869024,
-    'address': 'Phidim, Taplejung, Nepal'
-  },
-  {
-    'name': 'Itahari EV station',
-    'latitude': 26.680029761745857,
-    'longitude': 87.2356711873441,
-    'address': 'Itahari, Sunsari, Nepal'
-  },
-  {
-    'name': 'Birgunj EV station',
-    'latitude': 27.046464795619425,
-    'longitude': 84.90042375193129,
-    'address': 'Birgunj, Parsa, Nepal'
-  },
-  {
-    'name': 'Simara EV station',
-    'latitude': 27.163705338305512,
-    'longitude': 84.97952866274093,
-    'address': 'Simara, Bara, Nepal'
-  },
-  {
-    'name': 'Chandranigahapur EV station',
-    'latitude': 27.16805704267558,
-    'longitude': 85.35731328974694,
-    'address': 'Chandranigahapur, Rautahat, Nepal'
-  },
-  {
-    'name': 'Dhalkebar EV station',
-    'latitude': 26.9508791376429,
-    'longitude': 85.80012870516057,
-    'address': 'Dhalkebar, Dhanusha, Nepal'
-  },
-  {
-    'name': 'Janakpur EV station',
-    'latitude': 26.727328651509946,
-    'longitude': 85.94059939552218,
-    'address': 'Janakpur, Dhanusha, Nepal'
+    'name': 'Banepa EV station',
+    'latitude': 27.62983788196194,
+    'longitude': 85.52509728587273,
+    'address': 'Banepa, Kavre, Nepal'
   },
   {
     'name': 'Bardibas EV station',
@@ -90,46 +12,16 @@ List evStationsCoordinates = [
     'address': 'Bardibas, Mahottari, Nepal'
   },
   {
-    'name': 'Rajbiraj EV station',
-    'latitude': 26.510844742581412,
-    'longitude': 86.73792836446434,
-    'address': 'Rajbiraj, Saptari, Nepal'
+    'name': 'Bardia EV station',
+    'latitude': 28.272233958049207,
+    'longitude': 81.67197159498726,
+    'address': 'Bardia, Bardiya, Nepal'
   },
   {
-    'name': 'Malekhu Bazar EV station',
-    'latitude': 27.80970731264833,
-    'longitude': 84.8330241586965,
-    'address': 'Malekhu, Dhading, Nepal'
-  },
-  {
-    'name': 'Dhading EV station',
-    'latitude': 27.800341730468094,
-    'longitude': 84.87290522768122,
-    'address': 'Dhading Besi, Dhading, Nepal'
-  },
-  {
-    'name': 'Sindhuli Gadhi EV station',
-    'latitude': 27.280737479801832,
-    'longitude': 85.95503839738389,
-    'address': 'Sindhuli Gadhi, Sindhuli, Nepal'
-  },
-  {
-    'name': 'Selfie Danda EV station',
-    'latitude': 27.267396539222965,
-    'longitude': 85.9501353197192,
-    'address': 'Selfie Danda, Sindhuli, Nepal'
-  },
-  {
-    'name': 'Hetauda Park EV station',
-    'latitude': 27.442248069766745,
-    'longitude': 84.9990221635553,
-    'address': 'Hetauda, Makwanpur, Nepal'
-  },
-  {
-    'name': 'Tapoban EV station',
-    'latitude': 27.466801778887962,
-    'longitude': 84.91729719293386,
-    'address': 'Tapoban, Hetauda, Nepal'
+    'name': 'Bardaghat EV station',
+    'latitude': 27.552341336382344,
+    'longitude': 83.79820806855443,
+    'address': 'Bardaghat, Nawalparasi, Nepal'
   },
   {
     'name': 'Bharatpur EV station',
@@ -138,46 +30,76 @@ List evStationsCoordinates = [
     'address': 'Bharatpur, Chitwan, Nepal'
   },
   {
+    'name': 'Bindabasini EV station',
+    'latitude': 28.23801787463195,
+    'longitude': 83.98417831089921,
+    'address': 'Bindabasini Temple, Pokhara, Nepal'
+  },
+  {
+    'name': 'ElectriVa Charging Station',
+    'latitude': 26.49322,
+    'longitude': 87.27840,
+    'address': 'Biratnagar, Morang, Nepal'
+  },
+  {
+    'name': 'Electric Vechicle Charging station',
+    'latitude': 26.48786,
+    'longitude': 87.27706,
+    'address': 'Airport Mode Biratnagar, Morang, Nepal'
+  },
+  {
+    'name': 'Shree Shyam Power HUB',
+    'latitude': 26.47095,
+    'longitude': 87.28708,
+    'address': 'Oposite Shyam Mandir Biratnagar-2 , Morang, Nepal'
+  },
+  {
+    'name': 'Superior Service Center and Charging Station',
+    'latitude': 26.48341,
+    'longitude': 87.27352,
+    'address': 'Biratnagar, Morang, Nepal'
+  },
+  {
+    'name': 'NEA Charging Station',
+    'latitude': 26.46608,
+    'longitude': 87.28563,
+    'address': 'Biratnagar, Morang, Nepal'
+  },
+  {
+    'name': 'Birgunj EV station',
+    'latitude': 27.046464795619425,
+    'longitude': 84.90042375193129,
+    'address': 'Birgunj, Parsa, Nepal'
+  },
+  {
+    'name': 'Butwal EV station',
+    'latitude': 27.701689984957004,
+    'longitude': 83.44897610709061,
+    'address': 'Butwal, Rupandehi, Nepal'
+  },
+  {
+    'name': 'Chandragiri EV station',
+    'latitude': 26.576475130500278,
+    'longitude': 88.07778683051427,
+    'address': 'Chandragiri Hills, Thankot, Nepal'
+  },
+  {
+    'name': 'Chandranigahapur EV station',
+    'latitude': 27.16805704267558,
+    'longitude': 85.35731328974694,
+    'address': 'Chandranigahapur, Rautahat, Nepal'
+  },
+  {
     'name': 'Chitwan Medical College EV station',
     'latitude': 27.686091103349753,
     'longitude': 84.42996649319537,
     'address': 'Chitwan Medical College, Bharatpur, Nepal'
   },
   {
-    'name': 'Muglin EV station',
-    'latitude': 27.767280637327623,
-    'longitude': 84.4697639108882,
-    'address': 'Muglin, Chitwan, Nepal'
-  },
-  {
-    'name': 'Pathivara EV station',
-    'latitude': 27.76092938063862,
-    'longitude': 84.47507468475278,
-    'address': 'Pathivara, Chitwan, Nepal'
-  },
-  {
-    'name': 'Kurintar EV station',
-    'latitude': 27.877909887316335,
-    'longitude': 84.62122931089075,
-    'address': 'Kurintar, Gorkha, Nepal'
-  },
-  {
-    'name': 'Manakamana EV station',
-    'latitude': 27.872323837250203,
-    'longitude': 84.60081233598251,
-    'address': 'Manakamana, Gorkha, Nepal'
-  },
-  {
-    'name': 'Banepa EV station',
-    'latitude': 27.62983788196194,
-    'longitude': 85.52509728587273,
-    'address': 'Banepa, Kavre, Nepal'
-  },
-  {
-    'name': 'Nawalpur EV station',
-    'latitude': 27.6884687462591,
-    'longitude': 85.33402919609276,
-    'address': 'Nawalpur, Nawalparasi, Nepal'
+    'name': 'Civil Service EV station',
+    'latitude': 27.68656625538563,
+    'longitude': 85.33866987195383,
+    'address': 'Putalisadak, Kathmandu, Nepal'
   },
   {
     'name': 'Damauli EV station',
@@ -186,10 +108,124 @@ List evStationsCoordinates = [
     'address': 'Damauli, Tanahun, Nepal'
   },
   {
-    'name': 'Syangja EV station',
-    'latitude': 28.099805586494945,
-    'longitude': 83.871481900019,
-    'address': 'Syangja, Gandaki Province, Nepal'
+    'name': 'Dhading EV station',
+    'latitude': 27.800341730468094,
+    'longitude': 84.87290522768122,
+    'address': 'Dhading, Nepal'
+  },
+  {
+    'name': 'Dhalkebar EV station',
+    'latitude': 26.9508791376429,
+    'longitude': 85.80012870516057,
+    'address': 'Dhalkebar, Dhanusha, Nepal'
+  },
+  {
+    'name': 'Dhangadhi EV station',
+    'latitude': 28.697793709645504,
+    'longitude': 80.60211175280161,
+    'address': 'Dhangadhi, Kailali, Nepal'
+  },
+  {
+    'name': 'Hetauda Park EV station',
+    'latitude': 27.442248069766745,
+    'longitude': 84.9990221635553,
+    'address': 'Hetauda, Makwanpur, Nepal'
+  },
+  {
+    'name': 'NEA Charging  station',
+    'latitude': 26.67159,
+    'longitude': 87.28258,
+    'address': 'Taltalaiya Rd Itahari, Sunsari, Nepal'
+  },
+  {
+    'name': 'Tata Power Charging  station',
+    'latitude': 26.68735,
+    'longitude': 87.24209,
+    'address': 'Itahari, Sunsari, Nepal'
+  },
+  {
+    'name': 'Electric Vehicle Charging  station',
+    'latitude': 26.65992,
+    'longitude': 87.27605,
+    'address': 'Itahari, Sunsari, Nepal'
+  },
+  {
+    'name': 'Janakpur EV station',
+    'latitude': 26.727328651509946,
+    'longitude': 85.94059939552218,
+    'address': 'Janakpur, Dhanusha, Nepal'
+  },
+  {
+    'name': 'KMC EV station',
+    'latitude': 27.68459852890479,
+    'longitude': 85.41120564749042,
+    'address': 'Kathmandu, Nepal'
+  },
+  {
+    'name': 'Kathmandu Ev Station',
+    'latitude': 27.7172,
+    'longitude': 85.3240,
+    'address': 'Kathmandu, Nepal'
+  },
+  {
+    'name': 'Kurintar EV station',
+    'latitude': 27.877909887316335,
+    'longitude': 84.62122931089075,
+    'address': 'Kurintar, Chitwan, Nepal'
+  },
+  {
+    'name': 'Lumbini EV station',
+    'latitude': 27.481996233152434,
+    'longitude': 83.27671372451816,
+    'address': 'Lumbini, Rupandehi, Nepal'
+  },
+  {
+    'name': 'Malekhu Bazar EV station',
+    'latitude': 27.80970731264833,
+    'longitude': 84.8330241586965,
+    'address': 'Malekhu, Dhading, Nepal'
+  },
+  {
+    'name': 'Mahendranagar EV station',
+    'latitude': 28.92743395094924,
+    'longitude': 80.17519530220147,
+    'address': 'Mahendranagar, Kanchanpur, Nepal'
+  },
+  {
+    'name': 'Manakamana EV station',
+    'latitude': 27.872323837250203,
+    'longitude': 84.60081233598251,
+    'address': 'Manakamana, Gorkha, Nepal'
+  },
+  {
+    'name': 'Muglin EV station',
+    'latitude': 27.767280637327623,
+    'longitude': 84.4697639108882,
+    'address': 'Muglin, Chitwan, Nepal'
+  },
+  {
+    'name': 'Nepalgunj EV station',
+    'latitude': 28.101179195379782,
+    'longitude': 81.66737268390958,
+    'address': 'Nepalgunj, Banke, Nepal'
+  },
+  {
+    'name': 'Nawalpur EV station',
+    'latitude': 27.6884687462591,
+    'longitude': 85.33402919609276,
+    'address': 'Nawalpur, Nawalparasi, Nepal'
+  },
+  {
+    'name': 'Pathivara EV station',
+    'latitude': 26.57499655512588,
+    'longitude': 87.8302896869024,
+    'address': 'Pathivara, Taplejung, Nepal'
+  },
+  {
+    'name': 'Pathivara EV station',
+    'latitude': 27.76092938063862,
+    'longitude': 84.47507468475278,
+    'address': 'Pathivara, Tanahun, Nepal'
   },
   {
     'name': 'Pokhara Airport EV station',
@@ -204,57 +240,45 @@ List evStationsCoordinates = [
     'address': 'Lakeside, Pokhara, Nepal'
   },
   {
-    'name': 'Bindabasini EV station',
-    'latitude': 28.23801787463195,
-    'longitude': 83.98417831089921,
-    'address': 'Bindabasini Temple, Pokhara, Nepal'
+    'name': 'Rajbiraj EV station',
+    'latitude': 26.510844742581412,
+    'longitude': 86.73792836446434,
+    'address': 'Rajbiraj, Saptari, Nepal'
   },
   {
-    'name': 'Nepalgunj EV station',
-    'latitude': 28.101179195379782,
-    'longitude': 81.66737268390976,
-    'address': 'Nepalgunj, Banke, Nepal'
+    'name': 'Rupandehi EV station',
+    'latitude': 27.513738375195498,
+    'longitude': 83.45152909164501,
+    'address': 'Rupandehi, Nepal'
   },
   {
     'name': 'Surkhet EV station',
-    'latitude': 28.59731554138421,
-    'longitude': 81.6300992353532,
-    'address': 'Surkhet, Karnali Province, Nepal'
-  },
-  {
-    'name': 'Bardia EV station',
-    'latitude': 28.272233958049207,
-    'longitude': 81.67197159498726,
-    'address': 'Bardia, Bardiya, Nepal'
-  },
-  {
-    'name': 'Dhangadhi EV station',
-    'latitude': 28.697793709645504,
-    'longitude': 80.60211175280161,
-    'address': 'Dhangadhi, Kailali, Nepal'
-  },
-  {
-    'name': 'Mahendranagar EV station',
-    'latitude': 28.92743395094924,
-    'longitude': 80.17519530220147,
-    'address': 'Mahendranagar, Kanchanpur, Nepal'
-  },
-  {
-    'name': 'Lumbini EV station',
-    'latitude': 27.481996233152434,
-    'longitude': 83.27671372451816,
-    'address': 'Lumbini, Rupandehi, Nepal'
-  },
-  {
-    'name': 'Butwal EV station',
-    'latitude': 27.701689984957004,
-    'longitude': 83.44897610709061,
-    'address': 'Butwal, Rupandehi, Nepal'
+    'latitude': 28.16131019787997,
+    'longitude': 81.64529731905256,
+    'address': 'Surkhet, Surkhet, Nepal'
   },
   {
     'name': 'Tansen EV station',
-    'latitude': 27.86889116886286,
-    'longitude': 83.54639383280491,
+    'latitude': 27.97815237708456,
+    'longitude': 83.76383951554726,
     'address': 'Tansen, Palpa, Nepal'
+  },
+  {
+    'name': 'Tribhuvan International Airport EV station',
+    'latitude': 27.69698167561699,
+    'longitude': 85.35515596238784,
+    'address': 'Tribhuvan International Airport, Kathmandu, Nepal'
+  },
+  {
+    'name': 'Tulsipur EV station',
+    'latitude': 28.096825268963165,
+    'longitude': 82.66338260280422,
+    'address': 'Tulsipur, Dang, Nepal'
+  },
+  {
+    'name': 'Waling EV station',
+    'latitude': 27.94262895136821,
+    'longitude': 83.50345114977513,
+    'address': 'Waling, Syangja, Nepal'
   }
 ];
